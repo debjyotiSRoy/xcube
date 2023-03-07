b89ed2f2bbfd46622fe3e4b3e4883c67ae47604a
debjyotiSRoy <debjyoti.saharoy@gmail.com>
2023-03-07 16:56:33 -0500

    nn model for l2r just like fastai's tabular model

--- xcube/l2r/models/core.py
+++ xcube/l2r/models/core.py
@@ -5,7 +5,8 @@ __all__ = ['L2R_DotProductBias', 'L2R_NN']
 
 # %% ../../../nbs/07_l2r.models.core.ipynb 2
 from fastai.torch_imports import *
-from fastai.layers import sigmoid_range
+from fastai.layers import *
+from ...layers import *
 
 # %% ../../../nbs/07_l2r.models.core.ipynb 6
 class L2R_DotProductBias(nn.Module):
@@ -43,22 +44,38 @@ class L2R_DotProductBias(nn.Module):
 
 # %% ../../../nbs/07_l2r.models.core.ipynb 8
 class L2R_NN(nn.Module):
-    def __init__(self, num_lbs, num_toks, num_factors, n_act = 200, dp=0.0, y_range=None):
+    def __init__(self, num_lbs, 
+                 num_toks, 
+                 num_factors, 
+                 layers,
+                 ps=None,
+                 use_bn=True,
+                 bn_final=False,
+                 lin_first=True,
+                 embed_p=0.0,
+                 y_range=None):
         super().__init__()
         self.num_toks, self.num_lbs = num_toks+1, num_lbs+1 # +1 for the `padding_idx` 
-        self.num_factors, self.n_act, self.dp = num_factors, n_act, dp
+        self.num_factors, self.embed_p, self.ps = num_factors, embed_p, ps
         self.token_factors = nn.Embedding(self.num_toks, num_factors, padding_idx=-1)
         self.label_factors = nn.Embedding(self.num_lbs, num_factors, padding_idx=-1)
+        self.emb_drop = nn.Dropout(embed_p)
         self.y_range = y_range
-        self.layers = nn.Sequential(
-            nn.Linear(num_factors, n_act),
-            nn.ReLU(),
-            nn.Linear(n_act, 1),
-            nn.Dropout(self.dp),
-        )
+        if ps is None: self.ps = [0. for _ in range(len(layers))]
+        sizes = [self.token_factors.embedding_dim] + layers + [1]
+        actns = [nn.ReLU(inplace=True) for _ in range(len(sizes)-2)] + [None]
+        _layers = [LinBnFlatDrop(sizes[i], sizes[i+1], bn=use_bn and (i!=len(actns)-1 or bn_final), p=p, act=a, lin_first=lin_first)
+                   for i, (p,a) in enumerate(zip(self.ps+[0.],actns))]
+        self.layers= nn.Sequential(*_layers)
+        # self.layers = nn.Sequential(
+        #     nn.Linear(num_factors, n_act),
+        #     nn.ReLU(),
+        #     nn.Linear(n_act, 1),
+        #     nn.Dropout(self.ps),
+        # )
         
-    def __str__(self): return super().__repr__() + f"\n {self.n_act = }, {self.dp = }"
-    __repr__ = __str__
+    # def __str__(self): return super().__repr__() + f"\n {self.n_act = }, {self.embed_p = }"
+    # __repr__ = __str__
 
     
     def forward(self, xb):
@@ -76,6 +93,7 @@ class L2R_NN(nn.Module):
         
         # embs = torch.cat((toks_embs, lbs_embs), dim=-1) # shape (64, 2233, 64, 400)
         embs = toks_embs + lbs_embs
+        embs = self.emb_drop(embs)
         res = self.layers(embs)
         
         return sigmoid_range(res, *self.y_range) if self.y_range is not None else res
